27c1f4a69684b36b2ea26f8b4d7aeb1d247eec9f
muzzug <kadlec.ondrej@protonmail.com>
2025-02-16 14:42:30 +0100

    Přidání footnotes pluginu

--- src/_config/plugins.js
+++ src/_config/plugins.js
@@ -1,13 +1,20 @@
 import { EleventyI18nPlugin } from "@11ty/eleventy";
 import rssPlugin from "@11ty/eleventy-plugin-rss";
+import markdownIt from "markdown-it";
+import markdownItFootnote from "markdown-it-footnote";
 
 // Exporting plugins
 export const i18nPlugin = (eleventyConfig) => {
-    eleventyConfig.addPlugin(EleventyI18nPlugin, {
-      defaultLanguage: "cs",  
-      errorMode: 'allow-fallback'
-    });
-  };
-
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: "cs",
+    errorMode: "allow-fallback",
+  });
+};
 
 export const rss = rssPlugin;
+
+// Markdown plugin with footnotes support
+export const markdownPlugin = (eleventyConfig) => {
+  let markdownLib = markdownIt({ html: true }).use(markdownItFootnote);
+  eleventyConfig.setLibrary("md", markdownLib);
+};
